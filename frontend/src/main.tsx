65d828c3f7f57c54d02f0e42ea5874e874799451
Вадим <gubaydullin03internet.ru@MacBook-Air-Vadim-2.local>
2026-06-03 20:37:54 +0300

    fix4

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import { store } from './store'; // Путь к вашему стору
+import { store } from './store'; 
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </Provider>
