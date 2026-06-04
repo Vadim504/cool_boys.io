d5001330c79a139a1e6344007e6535fa307309a3
dooishe <tugievzx@mail.ru>
2026-06-04 14:13:11 +0300

    убрал base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import process from 'node:process'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "node:process";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const githubPagesBase = process.env.GITHUB_ACTIONS === 'true' && repositoryName
-  ? `/${repositoryName}/`
-  : '/'
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const githubPagesBase =
+  process.env.GITHUB_ACTIONS === "true" && repositoryName
+    ? `/${repositoryName}/`
+    : "/";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || githubPagesBase,
   plugins: [react()],
-  base: '/cool_boys.io/',
-})
+});
